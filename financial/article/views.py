f9e3a7b0f5bd6fc687b711343399799ce4f17b92
btolonq222 <btolonq222@hamicloud.net>
2019-06-19 22:05:38 +0800

    2019/06/19 part 2

--- financial/article/views.py
+++ financial/article/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from article.models import Article, Comment
 
 
 def article(request):
     """
     Render the page
     """
-    return render(request, 'article/article.html')
+    articles = {} 
+    for article in Article.objects.all():
+        articles.update({article:Comment.objects.filter(article=article)})
+    context = {'articles':articles}   #利用範本變數articles將查詢結果傳至範本
+    
+    return render(request, 'article/article.html', context)
 # Create your views here.
